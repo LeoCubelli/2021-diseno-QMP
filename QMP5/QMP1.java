d106f44059db2f0be99975c7f63d04eb67de5a69
Leonel Cubelli <leo.cube9801@gmail.com>
2021-05-27 00:26:12 -0300

    QMP5

---
+++ QMP5/QMP1.java
@@ -0,0 +1,72 @@
+
+public enum Categoria{
+    PARTE_SUPERIOR, PARTE_INFERIOR, CALZADO, ACCESORIO
+}
+
+public class Atuendo{
+    private Prenda parteSuperior;
+    private Prenda parteInferior;
+    private Prenda calzado;
+    private List<Prenda> accesorios;
+}
+
+public enum Material {
+    ALGODON, LANA, CUERO, PIQUE, ACETATO
+}
+
+public class TipoPrenda {
+    public static TipoPrenda ZAPATILLAS = new TipoPrenda(CALZADO);
+    public static TipoPrenda ZAPATOS = new TipoPrenda(CALZADO);
+    public static TipoPrenda REMERA = new TipoPrenda(PARTE_SUPERIOR);
+    public static TipoPrenda CAMISA = new TipoPrenda(PARTE_SUPERIOR);
+    public static TipoPrenda PANTALON = new TipoPrenda(PARTE_INFERIOR);
+
+    Categoria categoria;
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+}
+
+
+public class Prenda{
+    private TipoPrenda tipoPrenda;
+    private Material material;
+    private Color color;
+    private Color colorSecundario; 
+    private Trama trama;
+    private Temperatura temperatura;
+
+   public Prenda(String material, Color color, TipoPrenda tipoPrenda, Color colorSecundario, Trama trama, Temperatura temperatura){
+        this.tipoPrenda = tipoPrenda;
+        this.material = material;
+        this.color = color;
+        this.trama = trama;
+        this.colorSecundario = colorSecundario;
+        this.temperatura = temperatura;
+
+    public TipoPrenda getTipoPrenda() {
+        return tipoPrenda;
+    }
+
+    public Temperatura getTemperatura() {
+        return temperatura;
+    }
+}
+
+public class Color {
+    public static Color VERDE = new Color(0,0,255);
+    public static Color BLANCO = new Color(255,255,255);
+    public static Color NEGRO = new Color(0,0,0);
+    public static Color GRIS = new Color(130,130,130);
+
+    Integer nivelAzul;
+    Integer nivelRojo;
+    Integer nivelVerde;
+
+    public Color(Integer nivelAzul, Integer nivelRojo, Integer nivelVerde) {
+        this.nivelAzul = nivelAzul;
+        this.nivelRojo = nivelRojo;   
+        this.nivelVerde = nivelVerde;
+    }
+}
---
+++ QMP5/QMP5.java
@@ -0,0 +1,48 @@
+public abstract class Propuesta {
+  private Prenda prenda;
+  private Guardarropas guardarropa;
+
+}
+
+public class PropuestaPrendaAAgregar extends Propuesta {
+  public void aplicarPropuesta() {
+    guardarropa.cargarPrenda(prenda);
+  }
+}
+
+public class PropuestaPrendaAQuitar extends Propuesta {
+  public void aplicarPropuesta() {
+    guardarropa.sacarPrenda(prenda);
+  }
+}
+
+
+public class Usuario {
+  List<Guardarropas> guardarropas;
+  List<Propuesta> propuestas;
+  List<Propuesta> propuestasAceptadas;
+
+  public void compartirGuardarropas(Usuario usuario, Guardarropas guardarropa) {
+    usuario.agregarGuardarropas(guardarropa);
+  }
+
+  public void agregarGuardarropas(Guardarropas guardarropa) {
+    guardarropas.add(guardarropa)
+  }
+
+  public void agregarPropuesta(Propuesta propuesta) {
+    propuestas.add(propuesta)
+  }
+
+  public void rechazarPropuesta(Propuesta propuesta) {
+    if (propuestas.contains(propuesta)) {
+      propuestas.remove(propuesta);
+    }
+  }
+
+  public void aceptarPropuesta(Propuesta propuesta) {
+    propuesta.aplicarPropuesta();
+    propuestasAceptadas.add(propuesta);
+    propuestas.remove(propuesta);
+  }
+}
